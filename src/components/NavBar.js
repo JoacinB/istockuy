cd92b462859b967cf87a247f33b3d20ae44177e4
JoacinB <joaquinborgesborio@gmail.com>
2023-05-24 00:53:12 -0300

    Catalogo added and navbar shadow

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ function NavBar() {
   const { colorMode } = useColorMode();
   return (
     <Box
-      className="h-14 flex w-full fixed top-0 right-0 bg-transparent justify-center items-center"
-    >
+      className="h-14 flex w-full fixed top-0 right-0 bg-transparent backdrop-blur justify-center items-center"
+      boxShadow={colorMode === "dark" ? '5px 5px 30px -15px rgba(255,255,255,0.5)' : '5px 8px 30px -15px rgba(0,0,0,1)'}>
       <Flex className="flex-col h-full relative justify-center items-center">
         {/* <Link href="#home-section">
           <Tooltip
